81acb0960d7c01208570de11a2a7eba09847c50b
Abu Hasan <ranaabuhasan@gmail.com>
2021-09-13 16:19:51 +0600

    third commit

--- js/app.js
+++ js/app.js
@@ -13,19 +13,84 @@ const showProducts = (products) => {
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src="${image}">
-      </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
-      `;
+    div.innerHTML =
+      `<div class="single-product">
+           <div>
+         <img class="product-image" src="${image}">
+           </div>
+           <h3>${product.title}</h3>
+           <p class="fw-bold text-secondary">Category: ${product.category}</p>
+           <h2>Price: $ ${product.price}</h2>
+           <h5 class="fw-bold">Rating: ${product.rating.rate}
+           <br>
+           <i class="fas fa-star text-warning"></i>
+           <i class="fas fa-star text-warning"></i>
+           <i class="fas fa-star text-warning"></i>
+           <i class="fas fa-star text-warning"></i>
+           <i class="fas fa-star"></i>
+           <br>
+            Count:
+            <i class="fas fa-user"></i>
+            ${product.rating.count}</h5>
+           <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+           <button onclick="loadSingleProduct(${product.id})" id="details-btn" class="btn btn-danger">Details</button></div>
+           `;
+
+    /* `
+<div class="card h-100 single-product">
+          <img src="${image}" class="card-img-top product-image" alt="...">
+          <div class="card-body">
+            <h3>${product.title}</h3>
+            <p>Category: ${product.category}</p>
+            <h2>Price: $ ${product.price}</h2>
+            <p>Rating: ${product.rating.rate} Count: ${product.rating.count}</p>
+            <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+           <button onclick="loadSingleProduct(${product.id})" id="details-btn" class="btn btn-danger">Details</button></div>
+          </div>
+        </div>
+`; */
+
     document.getElementById("all-products").appendChild(div);
   }
 };
+
+/* load single Product */
+const loadSingleProduct = id => {
+  url = `https://fakestoreapi.com/products/${id}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displaySingle(data));
+}
+
+/* display single product */
+const displaySingle = product => {
+  document.getElementById('single-product').innerHTML = '';
+  const div = document.createElement('div');
+  div.classList.add('col', 'w-50', 'mx-auto');
+  div.innerHTML = `
+<div class="card h-100 single-product" style="background-color:antiquewhite">
+          <img src="${product.image}" class="card-img-top w-50 mx-auto" alt="...">
+          <div class="card-body">
+            <h3>${product.title}</h3>
+            <p class="fw-bold text-secondary">Category: ${product.category}</p>
+            <h2>Price: $ ${product.price}</h2>
+            <h5 class="fw-bold">Rating: ${product.rating.rate}
+           <br>
+           <i class="fas fa-star text-warning"></i>
+           <i class="fas fa-star text-warning"></i>
+           <i class="fas fa-star text-warning"></i>
+           <i class="fas fa-star text-warning"></i>
+           <i class="fas fa-star"></i>
+           <br>
+            Count:
+            <i class="fas fa-user"></i>
+            ${product.rating.count}</h5>
+          </div>
+        </div>
+`;
+  document.getElementById('single-product').appendChild(div);
+}
+
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
@@ -38,6 +103,7 @@ const addToCart = (id, price) => {
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
+  // const converted = parseFloat(element);
   const converted = parseInt(element);
   return converted;
 };
@@ -46,8 +112,10 @@ const getInputValue = (id) => {
 const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
-  const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  let total = convertedOldPrice + convertPrice;
+  total = total.toFixed(2);
+  document.getElementById(id).innerText = parseFloat(total);
+  // document.getElementById(id).innerText = Math.round(total);
 };
 
 // set innerText function
